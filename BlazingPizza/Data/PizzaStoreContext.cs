699dfadbfb234d62736e27280c16573465bd71f8
Tobi Ulm <tu@tobiasulm.net>
2025-06-03 12:33:38 +0200

    Projektverweise hinzugefügt und EF Core Installiert und PizzaStoreContext mit der Datenbankstruktur aufgebaut

---
+++ BlazingPizza/Data/PizzaStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BlazingPizza.Data
+{
+    public class PizzaStoreContext(DbContextOptions<PizzaStoreContext> options): DbContext(options)
+    {
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
+        public DbSet<Topping> Toppings { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<PizzaSpecial> Specials { get; set; } = null!;
+        public DbSet<Address> Address { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PizzaTopping>().HasKey(pt => new { pt.PizzaId, pt.ToppingId });
+            modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(pt => pt.Toppings);
+            modelBuilder.Entity<PizzaTopping>().HasOne(pst => pst.Topping).WithMany();
+            modelBuilder.Entity<Order>().OwnsOne(o => o.DeliveryLocation);
+        }
+    }
+}
