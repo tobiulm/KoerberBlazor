9cdcf7919cd43915bfdb73ee162007aead4ce477
Tobi Ulm <tu@tobiasulm.net>
2025-06-03 12:58:20 +0200

    Db Context zum DI Container hinzugefügt und erstellen der DB angestossen

--- BlazingPizza/Program.cs
+++ BlazingPizza/Program.cs
@@ -1,10 +1,18 @@
 using BlazingPizza.Components;
+using BlazingPizza.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add DB connection to DI Container
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
+builder.Services.AddDbContext<PizzaStoreContext>(options => { 
+    options.UseSqlite(connectionString);
+});
 
 var app = builder.Build();
 
@@ -15,8 +23,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    using var scope = app.Services.CreateScope();
+    var myDB = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    myDB.Database.EnsureCreated();
+}
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
